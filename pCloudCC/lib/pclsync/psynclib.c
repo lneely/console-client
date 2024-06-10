d688b57c31dee934364024a02d0973a31cde6539
Levi Neely <141506390+lneely@users.noreply.github.com>
2024-06-10 21:23:53 +0200

    removed unnecessary cast

--- pCloudCC/lib/pclsync/psynclib.c
+++ pCloudCC/lib/pclsync/psynclib.c
@@ -2866,7 +2866,7 @@ char* get_pc_name() {
 }
 /***********************************************************************************************************************************************/
 void psync_async_delete_sync(void* ptr) {
-  psync_syncid_t syncId = *(int *)(psync_syncid_t *)ptr;
+  psync_syncid_t syncId = *(psync_syncid_t *)ptr;
   int res;
 
   res = psync_delete_sync(syncId);
